90e719d023e21a4cfbb5d941f85417d59c071a18
jBeltrami <joaopbeltrami@gmail.com>
2023-11-15 17:51:45 -0300

    Add basic authentication using next-auth
    Use GoogleProvider to handle auth.
    Add layout changes with signIn/signOut functionality.

---
+++ app/NavMenu.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import AuthCheck from "./components/AuthCheck";
+import { SignInButton, SignOutButton } from "./components/buttons";
+
+export default async function NavMenu() {
+  return (
+    <nav>
+      <Link href={"/"}>
+        <Image src="/logo.svg" width={216} height={30} alt="Meu peso hoje" />
+      </Link>
+      <ul>
+        <li>
+          <Link href="/quem-somos">Quem somos</Link>
+        </li>
+        <li>
+          <SignInButton />
+        </li>
+        <li>
+          <AuthCheck>
+            <SignOutButton />
+          </AuthCheck>
+        </li>
+      </ul>
+    </nav>
+  );
+}
