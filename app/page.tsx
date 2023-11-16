a8109f2c671b706c579a7e5ebd30a05950bf9cea
jBeltrami <joaopbeltrami@gmail.com>
2023-11-16 10:25:46 -0300

    Add PrismaAdapter to fix JWT error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import styles from "./page.module.css";
 
 export default function Home() {
   const session = getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return <main className={styles.main}></main>;
 }
