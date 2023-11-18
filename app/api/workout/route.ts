ab1f2943b8960f900c913f1ff246887a4a469015
jBeltrami <joaopbeltrami@gmail.com>
2023-11-18 14:24:50 -0300

    Add routes to track workout (future feature)

---
+++ app/api/workout/route.ts
@@ -0,0 +1,38 @@
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+// GET ALL
+export async function GET(req: Request) {
+  const session = await getServerSession(authOptions);
+  const currentUser = session?.user;
+  const weightList = prisma.workout.findMany({ select: { id: true } });
+
+  return NextResponse.json(weightList);
+}
+
+// POST
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+  const currentUserEmail = session?.user?.email!;
+
+  const { treino, ontem } = await req.json();
+
+  const currentUserId = await prisma.user
+    .findUnique({ where: { email: currentUserEmail } })
+    .then((user) => user?.id!);
+
+  try {
+    const weight = await prisma.workout.create({
+      data: { userId: currentUserId, value: treino, date: ontem },
+    });
+
+    console.log("Created Weight:", weight);
+
+    return NextResponse.json(weight);
+  } catch (error) {
+    console.error("Error creating weight: ", error);
+    return NextResponse.error();
+  }
+}
